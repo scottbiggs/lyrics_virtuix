c8a17e000091beadedc5b5dadc7f3d672fd45842
sbiggs <scott.biggs@consensus-builder.com>
2024-05-27 18:49:26 -0500

    added check and appropriate ui logic.  added phrases to the strings.xml files (spanish and french too).

--- app/src/main/java/com/virtuix/lyricstats/MainScreen.kt
+++ app/src/main/java/com/virtuix/lyricstats/MainScreen.kt
@@ -2,16 +2,37 @@ package com.virtuix.lyricstats
 
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,6 +48,9 @@ object MainScreen {
 		uiState: MainUiState,
 		errState: ErrState
 	) {
+		/** Variable to control the switch.  True -> Most Used, False -> Longest word */
+		var checked by remember { mutableStateOf(false) }
+
 		LyricStatsTheme {
 
 			if (errState.errState) {
@@ -60,6 +84,59 @@ object MainScreen {
 						.fillMaxWidth()
 						.padding(all = 8.dp)
 				)
+
+
+				Row(
+					modifier = Modifier.fillMaxWidth(),
+					horizontalArrangement = Arrangement.Center
+				) {
+					Text(
+						stringResource(R.string.longest_word),
+						color = MaterialTheme.colorScheme.onSecondary,
+						modifier =
+							if (checked) {
+								Modifier
+									.align(Alignment.CenterVertically)
+									.padding(4.dp)
+							}
+							else {
+								Modifier
+									.align(Alignment.CenterVertically)
+									.background(MaterialTheme.colorScheme.secondary, shape = RoundedCornerShape(3.dp))
+									.border(2.dp, MaterialTheme.colorScheme.secondary, shape = RoundedCornerShape(3.dp))
+									.padding(4.dp)
+							}
+					)
+
+					// checked = most used, unchecked = longest word (default)
+					Switch(
+						checked = checked,
+						modifier = Modifier
+							.padding(start = 8.dp, end = 8.dp),
+						onCheckedChange = {
+							checked = it
+						}
+					)
+
+					Text(
+						stringResource(R.string.most_used_word),
+						modifier =
+							if (checked) {
+								Modifier
+									.align(Alignment.CenterVertically)
+									.background(MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(3.dp))
+									.border(2.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(3.dp))
+									.padding(4.dp)
+							}
+							else {
+								Modifier
+									.align(Alignment.CenterVertically)
+									.padding(4.dp)
+							}
+					)
+
+				}
+
 				Button(
 					onClick = viewModel::lookUpAndProcessLyrics,
 					modifier = Modifier
